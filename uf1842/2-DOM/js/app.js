5dd85a8a7cd01e77248674d1741dd44ffc8fb176
ccbaron <christian7baron@gmail.com>
2025-04-21 15:06:13 +0200

    Ejercicio DOM OK

--- uf1842/2-DOM/js/app.js
+++ uf1842/2-DOM/js/app.js
@@ -1,3 +1,23 @@
 /**
  * ¡Buena suerte! ¡Pasa el mensaje!
  */
+
+// Obtenemos los elementos que necesitamos del DOM
+
+const btnSend = document.getElementById('btn-send');
+const inputMessage = document.getElementById('message');
+const lastMessage = document.getElementById('last-message');
+
+// Asociamos el evento click al botón enviar
+
+btnSend.addEventListener('click', function() {
+
+    // 1. Tomamos el valor del input
+    const message = inputMessage.value;
+    
+    // 2. Colocamos el valor en el contenedor #last-message, modificando el DOM
+    lastMessage.textContent = message;
+    
+    // 3. Limpiamos el input
+    inputMessage.value = '';
+});
