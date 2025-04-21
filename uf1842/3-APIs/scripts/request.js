e500d5d9136e03538d72380f40291b71a3ed1c3c
ccbaron <christian7baron@gmail.com>
2025-04-21 17:48:38 +0200

    Ejercicio APIs OK

--- uf1842/3-APIs/scripts/request.js
+++ uf1842/3-APIs/scripts/request.js
@@ -1,5 +1,16 @@
 async function getPuzzle(wordCount) {
-    let hiddenSentence = "Buena Suerte";
 
-    return hiddenSentence;
+    // 1. Construimos la URL de la API con el parámetro wordCount
+    const apiUrl = `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`;
+
+    // 2. Hacemos la petición GET a la API usando fetch. Usamos await para esperar hasta que recibamos la respuesta del servidor
+    const response = await fetch(apiUrl);
+
+    // 3. Convertimos la respuesta de formato JSON a objeto JavaScript
+    // La API devuelve datos en formato JSON que debemos parsear
+    const data = await response.json();
+
+    // 4. Extraemos y devolvemos la propiedad 'puzzle' del objeto recibido
+    // Esta propiedad contiene la palabra aleatoria que nos envía la API
+    return data.puzzle;
 }
